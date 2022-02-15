af4512f048d088a010bb8230fdc2bb06b502006e
Emalcat <88663610+Emalcat@users.noreply.github.com>
2022-02-15 11:26:05 +0100

    i just need to revert

--- src/main/java/io/axoniq/demo/giftcard/GiftCardApp.java
+++ src/main/java/io/axoniq/demo/giftcard/GiftCardApp.java
@@ -1,18 +1,23 @@
 package io.axoniq.demo.giftcard;
 
+<<<<<<< HEAD
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+=======
+>>>>>>> parent of 6219dd3 (added mongodb support)
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.bson.Document;
 
 @SpringBootApplication
 public class GiftCardApp {
 
     public static void main(String[] args) {
         SpringApplication.run(GiftCardApp.class, args);
+<<<<<<< HEAD
         System.out.println("test");
+=======
+>>>>>>> parent of 6219dd3 (added mongodb support)
     }
 }
